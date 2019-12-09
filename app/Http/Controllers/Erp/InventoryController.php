181d4e8820a1c634df1a35beec9ef7c941b8a84e
wywangwei <wang1230yang1230@163.com>
2019-12-09 10:30:32 +0800

    as

--- app/Http/Controllers/Erp/InventoryController.php
+++ app/Http/Controllers/Erp/InventoryController.php
@@ -29,7 +29,7 @@ class InventoryController extends Controller
                 //深圳仓首页列表
                 return view('erp.inventory.SZ.index', compact('warehouse_id'));
             break;
-            case Inventory::YN_WAREHOUSE_ID:
+            case Inventory::YN_WAREHOUSE_IDgit:
                 //印尼仓首页列表
                 return view('erp.inventory.YN.index', compact('warehouse_id'));
             break;
